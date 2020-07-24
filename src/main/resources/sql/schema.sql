ed68d0f28da27dbfc446d536d9e4ecf8ccdfe4b8
Sameer Argade <sameer.argade@dws.com.au>
2020-07-24 19:33:14 +1000

    Tidied up

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -10,4 +10,4 @@ CREATE  TABLE user (
  
 insert  into `user`(`id`,`is_active`,`password`,`roles`,`user_name`)
 values (1,1,'user@123','ROLE_USER','user'),
-(2,1,'admin@123','ROLE_ADMIN','admin'); 
+(2,1,'admin@123','ROLE_ADMIN','admin');
